18ebed8e3ce35ee84d2b7fd6fc6d7a7647870405
gu-ri <119157021+gu-ri@users.noreply.github.com>
2025-08-03 10:40:24 +0900

    commit.

--- php/utils/combination.php
+++ php/utils/combination.php
@@ -21,3 +21,16 @@ function get_all_combination_sums(array $array): array
     }
     return $sums;
 }
+
+function culc_combination(int $n, int $r): int
+{
+    if ($n <= 0 || $r <= 0 || $n < $r) return 0;
+
+    $num1 = 1;
+    for ($i = 0; $i < $r; $i++) $num1 *= ($n - $i);
+
+    $num2 = 1;
+    for ($i = $r; $i >= 1; $i--) $num2 *= $i;
+
+    return intdiv($num1, $num2);
+}
